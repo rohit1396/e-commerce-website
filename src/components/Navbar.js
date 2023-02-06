dbf4c7eb6db91d949c0abbc331500cd2b7356871
rohit <rohitgaikwad726@gmail.com>
2023-02-06 09:00:42 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,15 @@ const Navbar = () => {
         E-<span>Shop</span>
       </div>
       <div className="nav_links">
-        <span className="nav_link">Home</span>
-        <span className="nav_link">About</span>
-        <span className="nav_link">Products</span>
-      </div>
-      <div className="nav_links">
-        <span className="nav_link">Add to cart</span>
-        <span className="nav_link">Login</span>
+        <div>
+          <span className="nav_link">Home</span>
+          <span className="nav_link">About</span>
+          <span className="nav_link">Products</span>
+        </div>
+        <div>
+          <span className="nav_link">Add to cart</span>
+          <span className="nav_link">Login</span>
+        </div>
       </div>
     </div>
   );
