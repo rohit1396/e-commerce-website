40a2a035bc287f5aab316624b660efe2777190d0
rohit <rohitgaikwad726@gmail.com>
2023-02-06 08:58:28 +0530

    app styling and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
