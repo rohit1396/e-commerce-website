894366a24a6ef94a21dc6ae783679006da6291ea
rohit <rohitgaikwad726@gmail.com>
2023-02-07 20:47:53 +0530

    new comp added

---
+++ src/components/FeaturedProduct.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FeaturedProduct = (data) => {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      {/* {data.map.object((item) => (
+        <img src={item.image} alt="" />
+      ))} */}
+    </div>
+  );
+};
+
+export default FeaturedProduct;
