93afef6b177af5495b56078d8202496e43e7b7b3
rohit <rohitgaikwad726@gmail.com>
2023-02-07 20:42:54 +0530

    styling and routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Products from "./components/Products";
 
 function App() {
   return (
@@ -14,6 +15,15 @@ function App() {
             <>
               <Navbar />
               <Home />
+            </>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <>
+              <Navbar />
+              <Products />
             </>
           }
         />
