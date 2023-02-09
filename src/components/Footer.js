4345a7bc1713a948a604ed03f93c29ec949fabda
rohit <rohitgaikwad726@gmail.com>
2023-02-09 10:46:02 +0530

    footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="footer_header">
+        E-<span>Shop</span> @ 2023 All rights reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
