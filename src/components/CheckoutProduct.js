779780664fb6a4cb43d973f3446cc032be9f3176
rohit <rohitgaikwad726@gmail.com>
2023-03-09 19:35:06 +0530

    responsive checkoutProduct

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductValue } from "../ProductContext";
 import "./CheckoutProduct.css";
 
 const CheckoutProduct = ({ product }) => {
+  const [readMore, setReadMore] = useState(false);
   const [{}, dispatch] = ProductValue();
 
   const removeItem = (id) => {
@@ -20,7 +21,13 @@ const CheckoutProduct = ({ product }) => {
       <div className="checkoutProduct_content">
         <p className="checkoutProduct_title">Title: {product.title}</p>
         <p className="checkoutProduct_description">
-          Description: {product.description}
+          Description:{" "}
+          {readMore
+            ? product.description
+            : `${product.description.substring(0, 200)}...`}
+          <span className="readMore" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </span>
         </p>
         <p className="checkoutProduct_category">Category: {product.category}</p>
         <p className="checkoutProduct_price">Price : ${product.price}</p>
